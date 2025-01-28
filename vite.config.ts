cdbbd27a0beaa09d6f73fc2a2cfdbb5a6932a5c7
babamovandrej <andrejbabamov@gmail.com>
2025-01-28 17:29:49 +0100

    Added packages and setup husky and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import stylelint from 'vite-plugin-stylelint';
+import react from '@vitejs/plugin-react-swc';
+import precss from 'precss';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    stylelint({
+      fix: true,
+      include: ['./src/**/*.css', './src/**/*.pcss'],
+      configFile: './.stylelintrc.json',
+      emitErrorAsWarning: true,
+    }),
+    EnvironmentPlugin('all'),
+  ],
+  css: {
+    postcss: {
+      plugins: [precss()],
+    },
+  },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+  },
+});
