f02fa78d32995c83b4ad9eb4115fd5609d037956
babamovandrej <andrejbabamov@gmail.com>
2025-02-03 11:11:36 +0100

    Format codebase

--- src/components/about/PartnerCard/PartnerCard.tsx
+++ src/components/about/PartnerCard/PartnerCard.tsx
@@ -1,23 +1,21 @@
-import { FC, memo } from "react";
-import classNames from "./PartnerCard.module.pcss";
+import { FC, memo } from 'react'
+import classNames from './PartnerCard.module.pcss'
 
-export type PartnerColor = 'chainlink' | 'concero';
+export type PartnerColor = 'chainlink' | 'concero'
 
 type PartnerCardProps = {
-    title: string;
-    logo: JSX.Element;
-    color: PartnerColor;
-};
+	title: string
+	logo: JSX.Element
+	color: PartnerColor
+}
 
 const PartnerCard: FC<PartnerCardProps> = ({ title, logo, color }) => (
-    <div className={classNames['partner-card']}>
-        <div className={`${classNames['partner-card__logo']} ${classNames[color]}`}>
-            {logo}
-        </div>
-        <div className={classNames['partner-card__title']}>
-            <p className="text-medium">{title}</p>
-        </div>
-    </div>
-);
+	<div className={classNames['partner-card']}>
+		<div className={`${classNames['partner-card__logo']} ${classNames[color]}`}>{logo}</div>
+		<div className={classNames['partner-card__title']}>
+			<p className="text-medium">{title}</p>
+		</div>
+	</div>
+)
 
-export default memo(PartnerCard);
+export default memo(PartnerCard)
