4fbc8f851f8503507baf6ec3a9db25f5301d130c
babamovandrej <andrejbabamov@gmail.com>
2025-02-07 19:19:31 +0100

    Update sdk link

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,7 @@
 export const externalLinks = {
 	documentation: 'https://docs.lanca.io/getting-started',
-	sdk: 'https://www.npmjs.com/package/lanca-sdk-demo',
+	sdk: 'https://www.npmjs.com/package/@lanca/sdk',
 	app: 'https://app.lanca.io',
 	discord: 'https://discord.gg/lanca',
 	twitter: 'https://x.com/lanca_io?lang=en',
-	bridgingFramwork: '/',
 }
