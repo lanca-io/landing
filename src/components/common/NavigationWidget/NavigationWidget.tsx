00dbbf70fa0f48cb4e7055b7f8610d8cd165532c
babamovandrej <andrejbabamov@gmail.com>
2025-02-10 10:24:59 +0100

    Update the navigation widget

--- src/components/common/NavigationWidget/NavigationWidget.tsx
+++ src/components/common/NavigationWidget/NavigationWidget.tsx
@@ -5,35 +5,42 @@ import classNames from './NavigationWidget.module.pcss'
 type WidgetSize = 'small' | 'large'
 
 type WidgetProps = {
-	title: string
-	size?: WidgetSize
-	link: string
-	disabled?: boolean
+    title: string
+    size?: WidgetSize
+    link: string
+    disabled?: boolean
 }
 
 const NavigationWidget: FC<WidgetProps> = ({ title, size = 'large', link, disabled = false }): JSX.Element => {
-	const rightArrow = useMemo(() => <RightArrow />, [])
+    const rightArrow = useMemo(() => <RightArrow />, [])
 
-	return (
-		<div className={`${classNames['card']} ${classNames[size]} ${disabled ? classNames['disabled'] : ''}`}>
-			<div className={classNames['title']}>
-				<p className="heading-xsmall">{title}</p>
-			</div>
-			<div className={classNames['action']}>
-				{!disabled ? (
-					<a href={link} target="_blank" rel="noreferrer">
-						<div className={`${classNames['icon__wrapper']} ${disabled ? classNames['disabled'] : ''}`}>
-							{rightArrow}
-						</div>
-					</a>
-				) : (
-					<div className={`${classNames['icon__wrapper']} ${disabled ? classNames['disabled'] : ''}`}>
-						{rightArrow}
-					</div>
-				)}
-			</div>
-		</div>
-	)
+    return (
+        <div className={`${classNames['card']} ${classNames[size]} ${disabled ? classNames['disabled'] : ''}`}>
+			{!disabled ? (
+                <a href={link} target="_blank" rel="noreferrer" className={classNames['link']}>
+                    <div className={classNames['title']}>
+                        <p className="heading-xsmall">{title}</p>
+                    </div>
+                    <div className={classNames['action']}>
+                        <div className={classNames['icon__wrapper']}>
+                            {rightArrow}
+                        </div>
+                    </div>
+                </a>
+            ) : (
+                <>
+                    <div className={classNames['title']}>
+                        <p className="heading-xsmall">{title}</p>
+                    </div>
+                    <div className={classNames['action']}>
+                        <div className={classNames['icon__wrapper']}>
+                            {rightArrow}
+                        </div>
+                    </div>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default NavigationWidget
