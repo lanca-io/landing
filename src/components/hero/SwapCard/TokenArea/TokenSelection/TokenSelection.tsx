ad28cc2a558bb469d760f79ed1b38def439027b1
babamovandrej <andrejbabamov@gmail.com>
2025-02-10 17:19:31 +0100

    Fixed font sizes and update cover image

--- src/components/hero/SwapCard/TokenArea/TokenSelection/TokenSelection.tsx
+++ src/components/hero/SwapCard/TokenArea/TokenSelection/TokenSelection.tsx
@@ -4,21 +4,33 @@ import { externalLinks } from '../../../../../constants/constants'
 import Badge from '../../../../common/Badge'
 import classNames from './TokenSelection.module.pcss'
 
-const SelectedTokenShape: FC = (): JSX.Element => {
+type TokenSelectionProps = {
+	token: string
+	chain: string
+	tokenLogoURL: string
+	chainLogoURL: string
+}
+
+type SelectedTokenShapeProps = {
+	token: string
+	chain: string
+}
+
+const SelectedTokenShape: FC<SelectedTokenShapeProps> = ({ token, chain }): JSX.Element => {
 	return (
 		<div className={classNames['selected-token']}>
-			<h4>USDC</h4>
-			<p>Polygon</p>
+			<h4>{token}</h4>
+			<p>{chain}</p>
 		</div>
 	)
 }
 
-const TokenSelection: FC = (): JSX.Element => {
+const TokenSelection: FC<TokenSelectionProps> = ({ token, chain, tokenLogoURL, chainLogoURL}): JSX.Element => {
 	return (
 		<a className={classNames['token-selection__button']} href={externalLinks.app} rel="noreferrer" target="_blank">
 			<div className={classNames['token-selection']}>
-				<Badge logoURL="/USDC.svg" chainURL="/Polygon.svg" />
-				<SelectedTokenShape />
+				<Badge logoURL={tokenLogoURL} chainURL={chainLogoURL} />
+				<SelectedTokenShape token={token} chain={chain} />
 			</div>
 			<TrailArrowRightIcon />
 		</a>
